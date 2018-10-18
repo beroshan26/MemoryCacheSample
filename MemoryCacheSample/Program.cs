32e6fdb7df921db7561f80d5fdc578b7ec104fe2
beroshan26 <32231715+beroshan26@users.noreply.github.com>
2018-10-18 15:37:10 +1100

    initial commit

--- MemoryCacheSample/Program.cs
+++ MemoryCacheSample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Caching;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MemoryCacheSample
@@ -11,6 +12,23 @@ namespace MemoryCacheSample
     {
         static void Main(string[] args)
         {
+            SimpleMemoryCache cacheTest = new SimpleMemoryCache();
+            cacheTest.SetObject("hw", "hello world");
+            cacheTest.SetObject("temp", "temp value");
+            Console.WriteLine("hw value is " + cacheTest.GetObject<string>("hw"));
+            Console.WriteLine("temp value is " + cacheTest.GetObject<string>("temp"));
+            Thread.Sleep(2000);
+            Console.WriteLine("hw value is " + cacheTest.GetObject<string>("hw"));
+            Console.WriteLine("temp value is " + cacheTest.GetObject<string>("temp"));
+            cacheTest.Remove("temp");
+            Thread.Sleep(3000);
+            Console.WriteLine("temp value is " + cacheTest.GetObject<string>("temp"));
+            Thread.Sleep(6000);
+            Console.WriteLine("hw value is " + cacheTest.GetObject<string>("hw"));
+            Thread.Sleep(20000);
+            Console.WriteLine("hw value is " + cacheTest.GetObject<string>("hw"));
+
+            Console.ReadLine();
         }
     }
 
